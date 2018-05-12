1ec765b0fcff61385a9ca077294de13781318b81
Binwei Fang <fangbinwei@qq.com>
2018-05-12 13:09:21 +0800

    wraping up

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 // import _ from 'lodash'
-import './assets/css/style.css'
-import Icon from './assets/img/icon.png'
 
 function component() {
   var element = document.createElement('div')
 
   element.innerHTML = [].join.call(['Hello', 'webpack', 'Sleepwalker'], ' ')
-  element.classList.add('hello')
-  
-  var myIcon = new Image()
-  myIcon.src = Icon
-
-  element.appendChild(myIcon)
   return element
 }
 
